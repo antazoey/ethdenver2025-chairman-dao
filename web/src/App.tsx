4afa2a8f3e73c46888d6207c59201d19f7d5cc38
slush <sam@apeworx.io>
2025-02-26 10:59:45 -0600

    chore: lint

--- web/src/App.tsx
+++ web/src/App.tsx
@@ -1,24 +1,12 @@
-//import { useState } from 'react'
-//import reactLogo from './assets/react.svg'
 import './App.css'
 import 'bootstrap/dist/css/bootstrap.min.css';
 import AccordionColumns from './components/AccordionColumns';
 import { Container, Row, Col } from 'react-bootstrap';
 
 function App() {
-  //const [count, setCount] = useState(0)
 
   return (
     <div className="App">
-      {/* <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1> */}
       <Container id="tasks">
         <Row>
           <Col xs={12} md={6} id="proposed-tasks">
@@ -30,17 +18,6 @@ function App() {
         </Row>
       </Container>
         
-      {/* <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p> */}
     </div>
   )
 }
