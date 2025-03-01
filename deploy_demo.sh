29b2dff81c93283b22e00da11acbb1178027e7b0
slush <sam@apeworx.io>
2025-02-28 23:50:15 -0600

    feat: estimates: added: done: dusted: feat

--- deploy_demo.sh
+++ deploy_demo.sh
@@ -16,12 +16,12 @@ ARGUMENTS="(record { \
     tasks = vec {\
       record { id = 0; title = \"Storms Abrewin' Folk Night\"; description = \"I upsold a few tickets to an upcoming show in the coffee shop, increasing revenue.\"; state = variant { Pending }; voting_power = record { health = 10; spirit = 0 }; claims = vec {}; notes = vec {}; ratings = vec {\
         record { principal \"${ALICE}\"; record { health = 8; spirit = 4 } }; \
-      }; }; \
-      record { id = 1; title = \"New TikTok marketing videos\"; description = \"Over the weekend, I made TikToks about our brand.\"; state = variant { Pending }; voting_power = record { health = 0; spirit = 0 }; claims = vec {}; notes = vec {}; ratings = vec {}; }; \
-      record { id = 2; title = \"Handled large electronic-order sale\"; description = \"I helped our largest customer supply coffee to ETHDenver.\"; state = variant { Pending }; voting_power = record { health = 0; spirit = 0 }; claims = vec {}; notes = vec {}; ratings = vec {}; }; \
-      record { id = 3; title = \"Fixed the water heater\"; description = \"We have hot water again!\"; state = variant { Open }; voting_power = record { health = 10; spirit = 0 }; claims = vec {}; notes = vec {}; ratings = vec {}; }; \
-      record { id = 4; title = \"Singing happy birthday\"; description = \"Anyone who sang happy birthday this week should claim this task\"; state = variant { Open }; voting_power = record { health = 0; spirit = 0 }; claims = vec {}; notes = vec {}; ratings = vec {}; }; \
-      record { id = 5; title = \"Game night\"; description = \"This task is for those who contributed to game night\"; state = variant { Open }; voting_power = record { health = 0; spirit = 0 }; claims = vec {}; notes = vec {}; ratings = vec {}; }; \
+      }; estimated_health = 8; estimated_spirit = 4 }; \
+      record { id = 1; title = \"New TikTok marketing videos\"; description = \"Over the weekend, I made TikToks about our brand.\"; state = variant { Pending }; voting_power = record { health = 0; spirit = 0 }; claims = vec {}; notes = vec {}; ratings = vec {}; estimated_health = 8; estimated_spirit = 4  }; \
+      record { id = 2; title = \"Handled large electronic-order sale\"; description = \"I helped our largest customer supply coffee to ETHDenver.\"; state = variant { Pending }; voting_power = record { health = 0; spirit = 0 }; claims = vec {}; notes = vec {}; ratings = vec {}; estimated_health = 8; estimated_spirit = 4  }; \
+      record { id = 3; title = \"Fixed the water heater\"; description = \"We have hot water again!\"; state = variant { Open }; voting_power = record { health = 10; spirit = 0 }; claims = vec {}; notes = vec {}; ratings = vec {}; estimated_health = 8; estimated_spirit = 4  }; \
+      record { id = 4; title = \"Singing happy birthday\"; description = \"Anyone who sang happy birthday this week should claim this task\"; state = variant { Open }; voting_power = record { health = 0; spirit = 0 }; claims = vec {}; notes = vec {}; ratings = vec {}; estimated_health = 8; estimated_spirit = 4  }; \
+      record { id = 5; title = \"Game night\"; description = \"This task is for those who contributed to game night\"; state = variant { Open }; voting_power = record { health = 0; spirit = 0 }; claims = vec {}; notes = vec {}; ratings = vec {}; estimated_health = 8; estimated_spirit = 4  }; \
     }; \
     proposals = vec {\
       record { id = 5; title = \"Upgrade frother\"; description = \"Frother 17A is getting really old. We should replace it.\"; state = variant { Open }; notes = \"It will code a thousand sand\"; health_only = false }; \
